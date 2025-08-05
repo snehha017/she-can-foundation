e0ee0ce38ad5357fad9acf02993ae50d6144f405
Sneha Lande <santoshilande017@gmail.com>
2025-08-05 13:22:09 +0530

    added api intern detail in backend while html fetches it and shows it on the intern dashboard. added a logout button and a working clock.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, redirect, url_for, jsonify
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -6,6 +6,14 @@ app.secret_key = 'your_secret_key'
 @app.route("/")
 def index():
     return render_template("signup.html", show='login')
+
+@app.route("/api/intern")
+def intern_data():
+    return jsonify({
+        "name" : "Sneha Lande",
+        "referralCode" : "snehalande2025",
+        "donations" : 5000
+    })
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -18,6 +26,10 @@ def signup():
 @app.route('/dashboard')
 def dashboard():
     return render_template("dashboard.html")
+
+@app.route('/login')
+def logout():
+    return render_template("signup.html") 
 
 if __name__ == "__main__":
     app.run(debug=True)
